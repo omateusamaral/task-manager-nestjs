0b6a1a70e19a8d9669122f503927dc107bbb0c52
MateusAmaraldev <mateus.amaral@opencircle.com.br>
2021-07-26 14:39:19 -0300

    feat: validation and error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskInterface, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -41,7 +41,11 @@ export class TasksService {
     return task;
   }
   getTaskById(id: string): TaskInterface {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+    return found;
   }
 
   deleteTask(id: string): void {
