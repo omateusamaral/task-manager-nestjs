221e05fb4394093a36e5c615d28cb78a66095e1c
omateusamaral <mateus.amaral018@gmail.com>
2021-08-01 15:54:37 -0300

    feat: handling errors and singup

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: `Password must has at least 1 upper case letter,1 lower case letter and 1 number or special character`,
+  })
+  @IsNotEmpty()
+  password: string;
+}
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,30 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { password, username } = authCredentialsDto;
+
+    const user = this.create({
+      password,
+      username,
+    });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        //duplicate username
+        throw new ConflictException('Username already Exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
