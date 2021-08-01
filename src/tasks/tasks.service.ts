3fbd40758715f8b20fb8e431d5ff4f9d2465e6f6
omateusamaral <mateus.amaral018@gmail.com>
2021-08-01 18:45:53 -0300

    feat: restrictions in tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -17,8 +17,13 @@ export class TasksService {
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     return this.tasksRepository.getTasks(filterDto, user);
   }
-  async getTaskById(id: string): Promise<Task> {
-    const taskFounded = await this.tasksRepository.findOne(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
+    const taskFounded = await this.tasksRepository.findOne({
+      where: {
+        id,
+        user,
+      },
+    });
 
     if (!taskFounded) {
       throw new NotFoundException(`Task with ID ${id} not found`);
@@ -30,15 +35,19 @@ export class TasksService {
     return this.tasksRepository.createTask(createTaskDto, user);
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete(id);
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
 
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID ${id} not found`);
     }
   }
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
 
     task.status = status;
 
