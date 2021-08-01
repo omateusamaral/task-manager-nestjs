14eca6c17dfd0017797589281e1b1686c9fc9ce1
omateusamaral <mateus.amaral018@gmail.com>
2021-08-01 16:13:10 -0300

    feat: hashing password with bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,15 +5,19 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from 'bcrypt';
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { password, username } = authCredentialsDto;
 
+    //hash password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
-      password,
       username,
+      password: hashedPassword,
     });
 
     try {
